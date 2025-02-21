7eae1f33f4b70edaced8748f6fcd4b587d492d5e
iguin777i <igor.moreira777i@gmail.com>
2025-02-21 18:46:00 -0300

    add page-details

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -4,6 +4,7 @@ import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import ProductHeader from "./components/products-header";
+import ProductDetails from "./components/products-details";
 
 interface ProductPageProps {
   params: { slug: string; productId: string };
@@ -13,17 +14,24 @@ const ProductPage = async ({ params }: ProductPageProps) => {
   const { slug, productId } = await params;
   const product = await db.product.findUnique({
     where: { id: productId },
+    include: { restaurant: {select:{
+        name: true,
+        avatarImageUrl: true,
+        slug: true,
+    }}},
   });
+
   if (!product) {
     return notFound();
   }
+  if (product.restaurant.slug.toUpperCase() !== slug.toUpperCase()) {
+    return notFound();
+  }
   return (
-    <>
+    <div className="flex h-full flex-col">
     <ProductHeader product={product}/>
-      <h1>Product Page</h1>
-      {slug}
-      {productId}
-    </>
+    <ProductDetails product={product} />
+    </div>
   );
 };
 
