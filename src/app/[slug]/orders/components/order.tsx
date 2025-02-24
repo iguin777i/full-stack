d3cd88b9ce2412eba46f9e9c2bef58a9dd26b7da
iguin777i <igor.moreira777i@gmail.com>
2025-02-23 21:28:54 -0300

    feat: resolver o prisma

--- src/app/[slug]/orders/components/order.tsx
+++ src/app/[slug]/orders/components/order.tsx
@@ -31,7 +31,7 @@ interface OrderListProps {
 }
 
 const getStatusLabel = (status: OrderStatus) => {
-  if (status === "FINALISADO") return "Finalizado";
+  if (status === "FINALIZADO") return "Finalizado";
   if (status === "EM_PREPARAÇÃO") return "Em preparo";
   if (status === "PENDENTE") return "Pendente";
   return "";
@@ -58,7 +58,7 @@ const OrderList = ({ orders }: OrderListProps) => {
         <Card key={order.id}>
           <CardContent className="space-y-4 p-5">
             <div
-              className={`w-fit rounded-full px-2 py-1 text-xs font-semibold text-white ${order.status === OrderStatus.FINALISADO ? "bg-green-500 text-white" : "bg-gray-200 text-gray-500"} `}
+              className={`w-fit rounded-full px-2 py-1 text-xs font-semibold text-white ${order.status === OrderStatus.FINALIZADO ? "bg-green-500 text-white" : "bg-gray-200 text-gray-500"} `}
             >
               {getStatusLabel(order.status)}
             </div>
